044ed9dc3b7340646b360a11d38d1fa3ad7ed391
IcespotT <hayan0705@gmail.com>
2024-10-14 14:23:27 +0200

    Завдання 1 на кількість повторень літери

---
+++ Homeworks/hw_lesson9/hw9.1.py
@@ -0,0 +1,19 @@
+# Скласти програму, яка буде рахувати, яка літера найчастіше зустрічається у вашому прізвищі.
+
+last_name = 'Каршина'
+
+last_name = last_name.lower()
+
+max_count = 0
+most_frequent_letter = ''
+
+for letter in last_name:
+        count = 0
+        for char in last_name:
+            if char == letter:
+                count += 1
+        if count > max_count:
+            max_count = count
+            most_frequent_letter = letter
+
+print(f'Найчастіше зустрічається "{most_frequent_letter}" вона повторювалася: {max_count}')
