af3536e779f3fa0e9fffd00ae22336f295759a4e
IcespotT <hayan0705@gmail.com>
2024-10-14 14:06:28 +0200

    Завдання 3 замінити всі цифри в рядку словом цифра

---
+++ Homeworks/hw_lesson9/hw9.3.py
@@ -0,0 +1,12 @@
+# Замінить всі цифри у рядку на слова "цифра".
+str = 'mega string s 1 cifroy'
+str2 = ''
+
+list_of_nums = '0123456789'
+
+for item in list:
+    if item in list_of_nums:
+        str2 += '(ЦИФРА)'
+    else:
+        str2 += item
+print(str2)
