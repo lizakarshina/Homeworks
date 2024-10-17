3592604fc1dcea9fec569b9a2c0943f114cd33c6
IcespotT <hayan0705@gmail.com>
2024-10-17 12:05:41 +0200

    Доробив завдання 2

--- Homeworks/hw_lesson10/2.py
+++ Homeworks/hw_lesson10/2.py
@@ -1,11 +1,28 @@
 # 2. Вигадати задачку з усіма методами множин та усіма математичними діями та вирішити її.
 # Задачка має бути на якусь тему (наприклад, склад або комп'ютерний магазин)
+# 'Dell', 'Apple', 'Acer'
 
-
-notebooks = {'Lenovo', 'HP', 'Dell', 'Apple', 'Acer'}
+notebooks = {'Lenovo', 'HP', }
 monitors = {'Dell', 'Samsung', 'LG', 'Acer', 'BenQ'}
 accessories = {'Мишка', 'Клавіатура', 'Чохол', 'Apple'}
 other = {'Принтер', 'Сканер', 'Мишка', 'Dell', 'HP'}
+
+# Add
+notebooks.add('Dell')
+notebooks.add('Apple')
+notebooks.add('Acer')
+monitors.add('AOC')
+accessories.add('USB HUB')
+
+
+# Remove
+
+accessories.remove('USB HUB')
+other.remove('Принтер')
+
+# discard
+notebooks.discard('Xiaomi')
+notebooks.discard('AOC')
 
 
 # Знайти та вивести всі елементи
