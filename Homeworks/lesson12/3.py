8b7d0b16f3751e4db79e2a4ebea837cee995902e
IcespotT <lutiycirkach@gmail.com>
2024-10-23 17:51:49 +0200

    Завдання 3

---
+++ Homeworks/lesson12/3.py
@@ -0,0 +1,14 @@
+# Зрозуміти, який тип даних зберігається та як прийти до знайомого типу даних:
+
+numbers = map(int, input("enter: ").split()) 
+
+
+# нічого не зрозумів, але цікаво
+
+print(numbers) # <map object at 0x0000024FCD0B6290>??
+print(list(numbers)) # [1, 2, 3, 4]
+
+# str = '1 2 3 4 5'
+# str = str.split()
+
+# print(int(str[0])) # [1, 2, 3, 4, 5]
