43214b78304e0fde00338781bcda9c6be6ff295f
IcespotT <lutiycirkach@gmail.com>
2025-01-09 14:49:31 +0100

    Урок + дз

---
+++ Tasks/lesson2x7/3.py
@@ -0,0 +1,30 @@
+# Підключення до сервісного аккаунту гугл клауд та гугл таблиць
+import gspread
+import pandas
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+# підключення до ?
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
+]
+
+
+# Підклбючення до сервісного аккаунту за допомогою ключа
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    "endless-beach-447312-i9-52a137f071b7.json", scope
+)
+
+
+# Вхід в аккаунт
+client = gspread.authorize(creds)
+
+# Підключення до таблиці
+data = client.open_by_key('1ifqh6r1qOJnytfNwJ2euU84fRbJ6ZKZFE7FI44gy-lo')
+
+worksheet = data.sheet1
+
+fr = pandas.DataFrame(worksheet.get_all_values())
+
+print(fr)
---
+++ Tasks/lesson2x7/4.py
@@ -0,0 +1,27 @@
+# Підключення до сервісного аккаунту гугл клауд та гугл таблиць
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+# підключення до ?
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
+]
+
+
+# Підклбючення до сервісного аккаунту за допомогою ключа
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    "endless-beach-447312-i9-52a137f071b7.json", scope
+)
+
+
+# Вхід в аккаунт
+client = gspread.authorize(creds)
+
+# Підключення до таблиці
+data = client.open_by_key('1ifqh6r1qOJnytfNwJ2euU84fRbJ6ZKZFE7FI44gy-lo')
+
+sheet1 = data.sheet1
+
+print(sheet1.get_all_values())
